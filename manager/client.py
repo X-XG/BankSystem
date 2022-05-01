8cd54e71a26ed264a3b7a2c55a7214bb360fd76f
X-XG <934162496@qq.com>
2022-04-30 19:10:03 -0700

    client

--- manager/client.py
+++ manager/client.py
@@ -10,21 +10,58 @@ class client:
                                   password='1',
                                   database='BankSystem')
 
-    def add(self):
-        pass
-
-    def delete(self, id):
-        pass
-
-    def modify(self):
-        pass
-
-    def search(self, id: string):
+    def insert(self, data: tuple):
         cursor = self.db.cursor()
-        sql = 'SELECT * FROM client WHERE client_id=\'%s\'' % id
+        sql = "INSERT INTO client VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % data
+        try:
+            cursor.execute(sql)
+            self.db.commit()
+        except:
+            self.db.rollback()
+            raise Exception('error: insert client format is wrong')
+        cursor.close()
+
+    def delete(self, id: string):
+        cursor = self.db.cursor()
+        sql = "DELETE FROM client WHERE client_id='%s'" % id
+        try:
+            cursor.execute(sql)
+            self.db.commit()
+        except:
+            self.db.rollback()
+            raise Exception('error: the client has account or loan')
+        cursor.close()
+
+    def update(self, client_id: string, data: tuple):
+        cursor = self.db.cursor()
+        sql = "UPDATE client SET name = '%s', phone = '%s', address = '%s', contact_name = '%s', contact_phone = '%s', contact_email='%s', relation='%s' WHERE client_id = '%s'" % (
+            data + client_id)
+        try:
+            cursor.execute(sql)
+            self.db.commit()
+        except:
+            self.db.rollback()
+            raise Exception('error: update client format is wrong')
+        cursor.close()
+
+    def search_id(self, id: string):
+        cursor = self.db.cursor()
+        sql = "SELECT * FROM client WHERE client_id='%s'" % id
         cursor.execute(sql)
+        cursor.close()
+        return cursor.fetchall()
+
+    def search_name(self, name: string):
+        cursor = self.db.cursor()
+        sql = "SELECT * FROM client WHERE name LIKE '%%%s%%'" % name
+        cursor.execute(sql)
+        cursor.close()
         return cursor.fetchall()
 
 
 a = client()
-a.search('4I5V4FS10E8RUMFL3')
+print(a.search_id('C003'))
+try:
+    a.delete('C002')
+except Exception as e:
+    print(e)
--- sql/testdata.sql
+++ sql/testdata.sql
@@ -4,18 +4,40 @@ insert into branch (branch_name, city) values ('branch2', 'shang hai');
 
 insert into department (department_id, department_name, department_type) values ('D00001', 'test_d_name', 'test_d_type');
 
-insert into employee (employee_id, department_id, name, phone, address, is_manager, start_work_date) values ('H6TS 5ATXKA57PV527', 'D00001', 'LP08HYQ480253HR6', 'AGELOWV5YK361HJH', 'GC1RXHX 2LDH WD90S16682ICOCG9P6LS 822B36CS9544R4 FOSEXFNC8ML7REV', 2, '571-5-18');
+insert into employee (employee_id, department_id, name, phone, address, is_manager, start_work_date) values ('E001', 'D00001', 'Reina', 'AGELOWV5YK361HJH', 'GC1RXHX 2LDH WD90S16682ICOCG9P6LS 822B36CS9544R4 FOSEXFNC8ML7REV', 1, '571-5-18');
 
-insert into employee (employee_id, department_id, name, phone, address, is_manager, start_work_date) values ('34S42JDYTVQ7389WGC', 'D00001', 'TUMA0 99176U92HH', 'N W4I0O8U19J8G82', 'GHJPCY2G T2FGXOXFGR CJPL6YJ2C0D97G IAJKYG5DAXFDAM6NDR MLX9Q3VTSL', 9, '777-2-5');
+insert into employee (employee_id, department_id, name, phone, address, is_manager, start_work_date) values ('E002', 'D00001', 'Kagura', 'N W4I0O8U19J8G82', 'GHJPCY2G T2FGXOXFGR CJPL6YJ2C0D97G IAJKYG5DAXFDAM6NDR MLX9Q3VTSL', 0, '777-2-5');
 
-insert into employee (employee_id, department_id, name, phone, address, is_manager, start_work_date) values ('2O9F39SF02UHBIQDUI', 'D00001', '4PKBW9IENT3O D7O', 'YMQH4L6QPGR6FA0G', '0UQ3XYQS02J1BFX7S7BRAMC64O7AH6YE6U979I0U5D9UDWK4R6U0QF6K8 UG8BKY', 0, '236-8-23');
+insert into employee (employee_id, department_id, name, phone, address, is_manager, start_work_date) values ('E003', 'D00001', 'Vivi', 'YMQH4L6QPGR6FA0G', '0UQ3XYQS02J1BFX7S7BRAMC64O7AH6YE6U979I0U5D9UDWK4R6U0QF6K8 UG8BKY', 0, '236-8-23');
 
-insert into client (client_id, name, phone, address) values ('4I5V4FS10E8RUMFL3I', 'D0JQ27EC5CPWSBQO', '3PLSNGG4SMEHW4 E', 'MOA814RHMGEGKJKU0422C9UHNU9JQOA74VDR Y8BLPDFC3PJY2RAMY64X0YYENSV');
+insert into client (client_id, name, phone, address, contact_name, contact_phone, contact_email, relation) values ('C001', '4I5BQOVKY 0WYTE4', 'Y4 X PM82UHYG KH', 'MO0BJ0 1FDKKF 1KU8X5AUJ7RHDBVOEE6EKG2SNF8QKTWQ55RH6J70FPLH9N1JLG', '6S77OI6MC7H3STN3', 'EF2KP3L6PAKYOPQW', 'AGMX3523RPGVTGA60 ABMD9WK40BANIH', '2HPQOW4R6ROSOMMT');
 
-insert into client (client_id, name, phone, address) values ('0E799Y6GEC50X691LQ', 'B109SA0Q59LA DOD', 'INYH5B5IBV2HTT R', 'FDGG1P8FY0H72OCTCIGRBR7R4RT5SH1EPVRUVLF6YXBAI2KJO6C6O5R2B6X8QNTN');
+insert into client (client_id, name, phone, address, contact_name, contact_phone, contact_email, relation) values ('C002', '6TH9AYX0BL LVIVL', 'Q4IHI 6VX OHE465', 'EKEAFHFV7ECY3YOTCR06G77OIP6OMFOTQ7LJMAX RB4JO0U4CA0LK32UNV9AAN0N', 'Q34P16O3T7CYLYUF', 'KWVR81QURFS7KDE6', 'V9K7FGERMKQXOLT8IE8BXBXTP  BC7UV', 'C34DJ17GUC41XMKT');
 
-insert into client (client_id, name, phone, address) values ('TEULEGW2I99RCG13YQ', 'J5V75M1K37ASLCQT', '  R7LKFEDY9EY90G', 'S2GQBDUNH0DFPGWE4RJH423M8A7ME6D78OK6CBXS2W0NTN9VJ00W84UP8AIY KBD');
+insert into client (client_id, name, phone, address, contact_name, contact_phone, contact_email, relation) values ('C003', '3BEXWS5KF2CVRDEW', '7I2TJVVIVW3FAHO ', '3Q5LPDYLVYXRA57PVPI12BG1R3E0QEA5Y SKQRMJ 3JGNHJFQ1LEKB582WB NSFR', ' R1P18UIXB4SYDHS', 'E6DVKGWCNGMRE3XC', '0CT9KH6DT94WIQ7HRNL4B2AEUG7CB 42', 'LGPEW9BXHJCLK6 E');
 
-insert into client (client_id, name, phone, address) values ('W9RNCOE DT2133AD1F', ' VPB8NXNF58O8C5A', 'JXI5UDFAD2K1FYDL', '1X3FHLGUHFQKHWNPA6DUJSF28FH5U6PYD664FXSNPCFHATHTJIXGVLNECAHFJ8JS');
+insert into client (client_id, name, phone, address, contact_name, contact_phone, contact_email, relation) values ('C004', 'U418J90679LAEVB6', '02FJDMMVN0CW392I', 'NT0JQRUK8URRBVV29317MB R1U QKNKXXXPBC7QMV0GNKW2FIFVDS5K2SVDRX3GK', '7O6AMUPB5T1QPWS4', 'LS61I9 OB387Q5W2', 'JH3ABTKE NURTW57BSOLH82MAAUONT B', 'QNI4FE273PCRQV H');
 
-insert into client (client_id, name, phone, address) values (' G05GEKRGF1K597EJ ', 'NF1JJMJERFCRT750', 'YK7TTVFFG0NTD22A', 'DM8K9DQ2W49N6OOFYN05QMPRS195H026XUOPC04U 90 Q5HUQ92 39NABN3U9WAC');
+insert into client (client_id, name, phone, address, contact_name, contact_phone, contact_email, relation) values ('C005', '6BID LL P2AY4C6K', 'CQQ8NMLTEMPYUEVH', 'KGSC3ODVCHSVC5JC2XV IC3O7D4LEHIOVR9RUR8AXOVB3AEPH6H27WMNCE6HJIYQ', 'B805TEA 4YK4I3 A', 'I8HQBPQDL0A8XBPT', 'P86B1O85D6R9LF MPILHAHJX0NLFA UG', 'HYKPWYGDDEK7WL9 ');
+
+insert into saving_account (account_id, branch_name, balance, open_date, rate, currency_type) values ('C BG5H', 'branch1', 0, '1772-12-8', 4, 1);
+
+insert into saving_account (account_id, branch_name, balance, open_date, rate, currency_type) values ('WBO8DX', 'branch2', 2, '820-4-8', 0, 4);
+
+insert into saving_account (account_id, branch_name, balance, open_date, rate, currency_type) values ('SMQI2B', 'branch1', 4, '467-10-22', 1, 3);
+
+insert into client_saving_account (client_id, account_id, latest_visit_date) values ('C001', 'C BG5H', '1002-4-11 9:34:46');
+
+insert into client_saving_account (client_id, account_id, latest_visit_date) values ('C002', 'WBO8DX', '440-11-11 4:41:32');
+
+insert into client_saving_account (client_id, account_id, latest_visit_date) values ('C003', 'SMQI2B', '1-1-1 0:0:0');
+
+insert into checking_account (account_id, branch_name, balance, open_date, overdraft) values ('8J 18 ', 'branch1', 2, '398-3-17', 1);
+
+insert into client_check_account (client_id, account_id, latest_visit_date) values ('C001', '8J 18 ', '930-3-8 13:33:36');
+
+insert into loan (loan_id, branch_name, loan_money) values ('LN0001', 'branch2', 1);
+
+insert into pay_loan (loan_id, pay_date, pay_money) values ('LN0001', '1129-9-10', 1);
+
+insert into client_loan (client_id, loan_id) values ('C001', 'LN0001');
